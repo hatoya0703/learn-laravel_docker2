c99bdd5a159493541d3ae5001f4bee0825145103
hatoya0703 <hatoya0703@gmail.com>
2023-06-04 22:54:18 +0900

    管理画面用のBlogクラスのcontrollerを作成

---
+++ app/Http/Controllers/Admin/AdminBlogController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminBlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // ブログ一覧画面を表示
+        // resources/views/admin/blogs/index.blade.phpを描画
+        return view('admin.blog.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // ブログ登録画面を表示
+        // resources/views/admin/blogs/create.blade.phpを描画 
+        return view('admin.blog.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
