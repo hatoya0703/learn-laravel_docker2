76ac6830ea430202d80e60687b6d000c1bf8eaf1
hatoya0703 <hatoya0703@gmail.com>
2023-06-09 09:54:33 +0900

    ルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,4 @@ Route::get('/contact/complete', [ContactController::class, 'complete'])->name('c
 
 Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index');
 Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
+Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
