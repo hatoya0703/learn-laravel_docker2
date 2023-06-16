2a339c4745f1f83d74ed9517ad37a09055fe1e25
hatoya0703 <hatoya0703@gmail.com>
2023-06-16 23:07:25 +0900

    Catのバリデーションルールを追加

--- app/Http/Requests/Admin/UpdateBlogRequest.php
+++ app/Http/Requests/Admin/UpdateBlogRequest.php
@@ -33,6 +33,7 @@ class UpdateBlogRequest extends FormRequest
                 'dimensions:min_width=100,min_height=100,max_width=1000,max_height=1000', // 画像サイズを指定
             ],
             'body' => ['required', 'string', 'max:2000'],
+            'cats.*' => ['distinct' , 'exists:cats,id'], // 重複していないこと、カテゴリーIDがcatsテーブルのidカラムに存在することを確認
         ];
     }
 }
