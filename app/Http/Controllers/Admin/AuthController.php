329fb8617717730da12bf162e00c7682057b9c5b
hatoya0703 <hatoya0703@gmail.com>
2023-06-17 21:28:44 +0900

    管理ページは全てログインが必要なように修正

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
 
             // ミドルウェアに対応したリダイレクト(後述)
             // 下記はredirect('/admin/blogs')に類似
-            return redirect()->intended('/admin/blogs');
+            return redirect()->intended('/admin/blogs'); // intendedメソッドはログイン前にアクセスしようとしていたページへリダイレクトする
         }
 
         // ログイン情報が正しくない場合のみ実行される処理(return すると以降の処理は実行されないため)
--- routes/web.php
+++ routes/web.php
@@ -26,14 +26,14 @@ Route::post('contact', [ContactController::class, 'sendmail']) ;
 Route::get('/contact/complete', [ContactController::class, 'complete'])->name('contact.complete');
 
 Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index')->middleware('auth');
-Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
-Route::post('/admin/blogs/store', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
-Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
-Route::put('/admin/blogs/{blog}', [AdminBlogController::class, 'update'])->name('admin.blogs.update');
-Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.delete');
+Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create')->middleware('auth');
+Route::post('/admin/blogs/store', [AdminBlogController::class, 'store'])->name('admin.blogs.store')->middleware('auth');
+Route::get('/admin/blogs/{blog}', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit')->middleware('auth');
+Route::put('/admin/blogs/{blog}', [AdminBlogController::class, 'update'])->name('admin.blogs.update')->middleware('auth');
+Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.delete')->middleware('auth');
 
-Route::get('/admin/users/create', [AdminUserController::class, 'create'])->name('admin.users.create');
-Route::post('/admin/users/store', [AdminUserController::class, 'store'])->name('admin.users.store');
+Route::get('/admin/users/create', [AdminUserController::class, 'create'])->name('admin.users.create')->middleware('auth');
+Route::post('/admin/users/store', [AdminUserController::class, 'store'])->name('admin.users.store')->middleware('auth');
 
 Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
 Route::post('/admin/login', [AuthController::class, 'login']);
