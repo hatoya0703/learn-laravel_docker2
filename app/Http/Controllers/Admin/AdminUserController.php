3dedf2a1c0dcb590136801ed3a8fefb611583339
hatoya0703 <hatoya0703@gmail.com>
2023-06-17 17:00:39 +0900

    ユーザー登録時の処理をコントローラーに追加

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\Admin\StoreUserRequest;
+use Illuminate\Support\Facades\Hash;
 
 class AdminUserController extends Controller
 {
@@ -27,9 +29,13 @@ class AdminUserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreUserRequest $request)
     {
-        //
+        $validated = $request->validated();
+        $validated['image'] = $request->file('image')->store('users', 'public');
+        $validated['password'] = Hash::make($validated['password']);
+
+        return back()->with('success', 'ユーザーを登録しました');
     }
 
     /**
