237f387f3ac128274b80fa496c91081423f4c530
hatoya0703 <hatoya0703@gmail.com>
2023-06-17 21:12:53 +0900

    ログイン、ログアウトのルーティングを設定

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Admin\AdminBlogController;
 use App\Http\Controllers\Admin\AdminUserController;
+use App\Http\Controllers\Admin\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,3 +34,7 @@ Route::delete('/admin/blogs/{blog}', [AdminBlogController::class, 'destroy'])->n
 
 Route::get('/admin/users/create', [AdminUserController::class, 'create'])->name('admin.users.create');
 Route::post('/admin/users/store', [AdminUserController::class, 'store'])->name('admin.users.store');
+
+Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/admin/login', [AuthController::class, 'login']);
+Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
